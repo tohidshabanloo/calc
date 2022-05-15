ccfa635425abfb529cb7d079ff7c7d93c77692a8
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-15 19:29:05 +0430

    just begin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Keys from "./components/Keys/Keys";
+
+function App() {
+  return (
+    <div className="App">
+      <Keys />
+    </div>
+  );
+}
+
+export default App;
