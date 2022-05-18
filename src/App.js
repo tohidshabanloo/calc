8d186707fe2ecebfbaf7142af27960e0beaf3882
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-18 18:29:59 +0430

    just copy :)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Keys from "./components/Keys/Keys";
+import React, {useState} from 'react';
+import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <Keys />
-    </div>
-  );
+const App =()=> {
+    const [result, setResult] = useState("")
+    const calculate = () => {
+        var checkResult = ''
+        if (result.includes('--')) {
+            checkResult = result.replace('--', '+')
+        } else {
+            checkResult = result
+        }
+
+        try {
+            // let stringRes = (eval(checkResult) || "").toString()
+            let stringRes = Function('"use strict";return ('+ (checkResult||"") +')')().toString()
+            setResult(stringRes)
+        } catch (e) {
+            setResult("error")
+        }
+    }
+    const reset = () => {
+        setResult("")
+    }
+    const backspace = () => {
+        console.log(result)
+        setResult(prevState => prevState.slice(0, -1))
+    }
+    const onClick = (button) => {
+        if (button === "=") {
+            calculate()
+        } else if (button === "C") {
+            reset()
+        } else if (button === "CE") {
+            backspace()
+        } else {
+            setResult(prevState => prevState + button)
+        }
+    }
+
+    return (
+        <div className={`App`}>
+            <div className="calculator-body">
+                <ResultComponent result={result}/>
+                <KeyPadComponent onClick={onClick}/>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
---
+++ src/components/ResultComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResultComponent = ({result}) => {
+    return (
+        <div className="result">
+            <p>{result}</p>
+        </div>
+    )
+}
+
+export default ResultComponent;
+
