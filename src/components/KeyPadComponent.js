8980d6321443021866699286803cf0aba9239792
tohidshabanloo <tshabanloo@hotmail.com>
2022-05-18 19:44:08 +0430

    somechanges

--- src/components/KeyPadComponent.js
+++ src/components/KeyPadComponent.js
@@ -1,43 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const KeyPadComponent = ({onClick}) => {
-    return (
-        <div className="button">
-            <button className={`actions`} name="(" onClick={e => onClick(e.target.name)}>(</button>
-            <button className={`actions`} name="CE" onClick={e => onClick(e.target.name)}>CE</button>
-            <button className={`actions`} name=")" onClick={e => onClick(e.target.name)}>)</button>
-            <button className={`actions`} name="C" onClick={e => onClick(e.target.name)}>C</button>
-            <br/>
-
-
-            <button name="1" onClick={e => onClick(e.target.name)}>1</button>
-            <button name="2" onClick={e => onClick(e.target.name)}>2</button>
-            <button name="3" onClick={e => onClick(e.target.name)}>3</button>
-            <button className={`actions`} name="+" onClick={e => onClick(e.target.name)}>+</button>
-            <br/>
-
-
-            <button name="4" onClick={e => onClick(e.target.name)}>4</button>
-            <button name="5" onClick={e => onClick(e.target.name)}>5</button>
-            <button name="6" onClick={e => onClick(e.target.name)}>6</button>
-            <button className={`actions`} name="-" onClick={e => onClick(e.target.name)}>-</button>
-            <br/>
-
-            <button name="7" onClick={e => onClick(e.target.name)}>7</button>
-            <button name="8" onClick={e => onClick(e.target.name)}>8</button>
-            <button name="9" onClick={e => onClick(e.target.name)}>9</button>
-            <button className={`actions`} name="*" onClick={e => onClick(e.target.name)}>x</button>
-            <br/>
-
-
-            <button name="." onClick={e => onClick(e.target.name)}>.</button>
-            <button name="0" onClick={e => onClick(e.target.name)}>0</button>
-            <button className={`actions equal`} name="=" onClick={e => onClick(e.target.name)}>=</button>
-            <button className={`actions`} name="/" onClick={e => onClick(e.target.name)}>÷</button>
-            <br/>
-        </div>
-    );
-}
-
+const KeyPadComponent = ({ onClick }) => {
+  return (
+    [
+      { name: "(", value: "(", className: "action" },
+      { name: "CE", value: "CE", className: "action" },
+      { name: ")", value: ")", className: "action" },
+      { name: "C", value: "C", className: "action" },
+    ],
+    [
+      { name: "1", value: "1", className: "" },
+      { name: "2", value: "2", className: "" },
+      { name: "3", value: "3", className: "" },
+      { name: "+", value: "C", className: "action" },
+    ],
+    [
+      { name: "4", value: "4", className: "" },
+      { name: "5", value: "5", className: "" },
+      { name: "6", value: "6", className: "" },
+      { name: "-", value: "-", className: "action" },
+    ],
+    [
+      { name: "7", value: "7", className: "" },
+      { name: "8", value: "8", className: "" },
+      { name: "9", value: "9", className: "" },
+      { name: "*", value: "x", className: "action" },
+    ],
+    [
+      { name: ".", value: "7", className: "" },
+      { name: "0", value: "8", className: "" },
+      { name: "=", value: "9", className: "" },
+      { name: "/", value: "/", className: "action" },
+    ]
+  );
+};
 
 export default KeyPadComponent;
